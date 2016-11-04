cf512fc71f262678197b8c84e6c7c10c17087a4d
Jon Small <jonssmall@gmail.com>
2016-11-04 15:29:55 -0400

    async reroute if unauthorized

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,12 +7,28 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../containers/Main');
 var HomeContainer = require('../containers/HomeContainer');
 var ProfileContainer = require('../containers/ProfileContainer');
+var auth = require('../helpers/authHelpers');
+
+function requireAuth (nextState, replace, callback) {
+    auth.isSignedOn()
+        .then(result => {
+            console.log(result); 
+            if(!result.data) {
+                console.log('not logged in');
+                replace({
+                    pathname: '/',
+                    state: { nextPathname: nextState.location.pathname }
+                });
+            }
+            callback();
+        });
+}
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>        
       <IndexRoute component={HomeContainer} />
-      <Route path='profile' component={ProfileContainer} />
+      <Route path='profile' component={ProfileContainer} onEnter={requireAuth}/>
     </Route>
   </Router>
 );

790ae3694963ab90e96c9276364f2c1ba2082f60
Jon Small <jonssmall@gmail.com>
2016-11-08 02:10:22 -0500

    beginning to do new poll state container

--- app/containers/NewPollContainer.js
+++ app/containers/NewPollContainer.js
@@ -6,13 +6,23 @@ function NewPoll (props) {
             <form>
                 <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <input 
-                        onChange={props.onUpdate}
+                        onChange={props.onUpdate.bind(null, 'test')}
                         value={props.title}
                         className="mdl-textfield__input" 
                         type="text" 
-                        id="fullUrl" />
-                    <label className="mdl-textfield__label" htmlFor="fullUrl">Title</label>                    
-                </div>                
+                        id="title" />
+                    <label className="mdl-textfield__label" htmlFor="title">Title</label>                    
+                </div>
+                <br />
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input className="mdl-textfield__input" type="text" id="option1"/>
+                    <label className="mdl-textfield__label" htmlFor="option1">Option1</label>                    
+                </div>
+                <br />
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input className="mdl-textfield__input" type="text" id="option2"/>
+                    <label className="mdl-textfield__label" htmlFor="option2">Option2</label>                    
+                </div>                     
             </form>            
         </div>
     )
@@ -24,20 +34,25 @@ var NewPollContainer = React.createClass({
     },
     getInitialState: function () {
         return {
-            title: ''
+            title: '',
+            options: {
+                option1: '',
+                option2: ''
+            }
         }
     },
     handleSubmit: function() {
         //console.log(this.state.city);
         //this.context.router.push('/forecast/' + this.state.city)       
     },
-    handleUpdate: function(e){                
+    handleUpdate: function(name, e){      
+        console.log(name);          
         this.setState({
             title: e.target.value
         });
     },        
     render: function () {
-        return <NewPoll onUpdate={this.handleUpdate} title={this.state.title} />
+        return <NewPoll onUpdate={this.handleUpdate} title={this.state.title} options={this.state.options} />
     }
 });
 

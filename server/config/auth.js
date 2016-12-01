2e76bbbead322792e2eca4abd7cbfc7baf34b3a7
Jon Small <jonssmall@gmail.com>
2016-11-30 22:20:17 -0500

    tweaking env configs

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,9 +1,11 @@
 'use strict';
 
+let callbackURL = process.env.APP_URL ? process.env.APP_URL + 'auth/github/callback' : process.env.path + 'auth/github/callback';
+
 module.exports = {
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+		'callbackURL': callbackURL
 	}
 };
